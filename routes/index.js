68dc364c765e5ecce496d0afb1aca37c3c19a254
LynnT <LynnTCai@gmail.com>
2024-01-13 03:11:10 -0600

    oAuth

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
